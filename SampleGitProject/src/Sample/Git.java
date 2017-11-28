b1b36668fe4aaaffeccb287109ee541c61beb539
acer <acer@acer-PC>
2017-11-28 07:05:17 +0530

    First checkin for the Sample project

---
+++ SampleGitProject/src/Sample/Git.java
@@ -0,0 +1,18 @@
+package Sample;
+
+public class Git {
+
+	public static void main(String[] args) {
+		Add();
+		
+
+	}
+	public static void Add(){
+		
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println(c);
+	}
+
+}
